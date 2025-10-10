d342bcc1dcf42f5c2abe861113ca1759b9f4141c
ninafang123 <n.fang1@students.uu.nl>
2025-10-10 19:32:08 +0200

    starting method1, not working

--- Src/main.py
+++ Src/main.py
@@ -3,6 +3,17 @@ import os
 import pandas as pd
 from data_generator import create_subsets
 from query_generator import generate_queries
+from method1 import method1
+
+def load_queries(file_path):
+     # Reading generated queries
+    with open(file_path, "r") as file:
+        queries = file.readlines()
+    # Remove newline characters
+    queries = [line.strip() for line in queries]
+    
+    print(f"Loaded {len(queries)} queries")
+    return queries
 
 file_path = "../Data/marketing_campaign_converted.csv"
 if os.path.exists(file_path):
@@ -11,3 +22,6 @@ create_subsets()
 print("Created the subset files based on marketing_campaign_converted.csv")
 
 generate_queries(10, 2)
+
+method1("../Data/tmp_small.csv", 30, "../Results/tmp_m1_small.csv", load_queries("../Data/queries.txt"))
+
