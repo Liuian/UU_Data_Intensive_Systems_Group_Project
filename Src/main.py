f591e2ae3ca81ecc43c2f76671acdc5c5a8d9163
ninafang123 <75363077+ninafang123@users.noreply.github.com>
2025-10-26 13:54:57 +0100

    def to test method 1

--- Src/main.py
+++ Src/main.py
@@ -12,36 +12,49 @@ def load_queries(queries_file):
     print(f"Loaded {len(queries)} queries from {queries_file}")
     return queries
 
-# Create subsets if the main dataset exists
-file_path = "../Data/marketing_campaign_converted.csv"
-if os.path.exists(file_path):
-    print(f"File {file_path} exists, proceeding to create subsets.")
 
-# Phase 1: small datasets
-subset_list = create_subsets(small=5, medium=10, large=20)
-print("Created the subset files based on marketing_campaign_converted.csv")
+def experiments():
+    # Create subsets if the main dataset exists
+    file_path = "../Data/marketing_campaign_converted.csv"
+    if os.path.exists(file_path):
+        print(f"File {file_path} exists, proceeding to create subsets.")
 
-queries_num = [50, 200]
-conditions = [1,3]
-t_values = [1,2,4]
+    # Phase 1: small datasets
+    subset_list = create_subsets(small=5, medium=10, large=20)
+    print("Created the subset files based on marketing_campaign_converted.csv")
 
-spark = SparkSession.builder.appName("Phase1").getOrCreate()
-for dataset in subset_list:
-    for q in queries_num:
-        for c in conditions:
-            # Generate queries for every dataset and save to text file
-            queries = generate_queries_weighted(
-                spark=spark,
-                csv_file_path=dataset,
-                num_queries=q,
-                max_conditions=c
-            )
+    queries_num = [50, 200]
+    conditions = [1,3]
+    t_values = [1,2,4]
 
-            for T in t_values:
-                base_name = os.path.basename(dataset).split('.')[0]
-                # Some code to measure our parameters
-                method1(spark, dataset, T, f"../Results/m1_{base_name}_q{q}_c{c}_T{T}.csv", load_queries(queries))
-                #call other methods 
-spark.stop()
-# Phase 2: large datasets
+    spark = SparkSession.builder.appName("Phase1").getOrCreate()
+    for dataset in subset_list:
+        for q in queries_num:
+            for c in conditions:
+                # Generate queries for every dataset and save to text file
+                queries = generate_queries_weighted(
+                    spark=spark,
+                    csv_file_path=dataset,
+                    num_queries=q,
+                    max_conditions=c
+                )
+
+                for T in t_values:
+                    base_name = os.path.basename(dataset).split('.')[0]
+                    # Some code to measure our parameters
+                    method1(spark, dataset, T, f"../Results/m1_{base_name}_q{q}_c{c}_T{T}.csv", load_queries(queries))
+                    #call other methods 
+    spark.stop()
+    # Phase 2: large datasets
+
+def testMethod1(file_path):
+    spark = SparkSession.builder.appName("TestMethod1").getOrCreate()
+    queries = generate_queries_weighted(
+                    spark=spark,
+                    csv_file_path=file_path,
+                    num_queries=20,
+                    max_conditions=3
+                )
+    method1(spark, file_path, 100, "../Results/m1_test.csv", load_queries(queries))
+    spark.stop()
 
