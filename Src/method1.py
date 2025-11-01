137b43eab7ab861316f4c7d182dd07e7c138e3b3
ninafang123 <n.fang1@students.uu.nl>
2025-11-01 11:42:30 +0100

    separate function for m1 to reuse in m2

--- Src/method1.py
+++ Src/method1.py
@@ -3,18 +3,16 @@ from pyspark.sql.functions import col, lit, when, array, expr, size, desc
 from utils import compute_query_coverage, compute_diversity
 import time
 
-
-
-def method1(spark, input_file, T, output_file, queries):
-    # Store original queries for coverage calculation
-    original_queries = queries.copy()
-
+def topT_method1(spark, input_file, T, queries):
+    """
+    Select top T tuples based on total hits from queries.
+    """
     start_time = time.time()
 
     # Read input data
     df = spark.read.csv(input_file, header=True, inferSchema=True)
     print(f"Loaded {df.count()} rows")
-    
+
     # Assign QID's for every Query
     queries_with_ids = [(qid+1, q) for qid, q in enumerate(queries)]
 
@@ -28,10 +26,19 @@ def method1(spark, input_file, T, output_file, queries):
 
     # Select top T rows by total_hits
     df_topT = df_total_hits.orderBy(desc("total_hits")).limit(T)
+    total_time = time.time() - start_time
+
+    return df_topT, total_time
+
+def method1(spark, input_file, T, output_file, queries):
+    # Store original queries for coverage calculation
+    original_queries = queries.copy()
+
+    df_topT, total_time = topT_method1(spark, input_file, T, queries)
 
     # Save to CSV
     df_topT.write.csv(output_file, header=True, mode='overwrite')
-    end_time = time.time()
+
 
     print(f"Saved top {T} tuples to {output_file}")
     
@@ -44,6 +51,6 @@ def method1(spark, input_file, T, output_file, queries):
     return {
         'imp_R': imp_R2, # Sum of total hits in R'
         'diversity': diversity,
-        'runtime': end_time - start_time,
+        'runtime': total_time,
         'query_coverage': query_coverage
     }
