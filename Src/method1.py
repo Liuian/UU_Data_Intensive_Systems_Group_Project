cac34b893767f5f4be075dedd58a18094670692b
ninafang123 <n.fang1@students.uu.nl>
2025-11-01 11:55:31 +0100

    pop_method1 return all tuples with their popularity

--- Src/method1.py
+++ Src/method1.py
@@ -3,15 +3,10 @@ from pyspark.sql.functions import col, lit, when, array, expr, size, desc
 from utils import compute_query_coverage, compute_diversity
 import time
 
-def topT_method1(spark, input_file, T, queries):
+def pop_method1(df, queries):
     """
     Select top T tuples based on total hits from queries.
     """
-    start_time = time.time()
-
-    # Read input data
-    df = spark.read.csv(input_file, header=True, inferSchema=True)
-    print(f"Loaded {df.count()} rows")
 
     # Assign QID's for every Query
     queries_with_ids = [(qid+1, q) for qid, q in enumerate(queries)]
@@ -25,22 +20,24 @@ def topT_method1(spark, input_file, T, queries):
     df_total_hits = df.withColumn("total_hits", total_hits_expr)
 
     # Select top T rows by total_hits
-    df_topT = df_total_hits.orderBy(desc("total_hits")).limit(T)
-    total_time = time.time() - start_time
-
-    return df_topT, total_time, df
+    df_pop = df_total_hits.orderBy(desc("total_hits"))
+    
+    return df_pop
 
 def method1(spark, input_file, T, output_file, queries):
     # Store original queries for coverage calculation
     original_queries = queries.copy()
+    start_time = time.time()
 
-    df_topT, total_time, df = topT_method1(spark, input_file, T, queries)
+    # Read input data
+    df = spark.read.csv(input_file, header=True, inferSchema=True)
 
+    # Get top T tuples based on popularity
+    df_topT = pop_method1(df, queries).limit(T)
+
+    end_time = time.time()
     # Save to CSV
     df_topT.write.csv(output_file, header=True, mode='overwrite')
-
-
-    print(f"Saved top {T} tuples to {output_file}")
     
     # Use original queries (without IDs) for coverage calculation
     query_coverage = compute_query_coverage(spark, df_topT, original_queries)
@@ -51,6 +48,6 @@ def method1(spark, input_file, T, output_file, queries):
     return {
         'imp_R': imp_R2, # Sum of total hits in R'
         'diversity': diversity,
-        'runtime': total_time,
+        'runtime': end_time - start_time,
         'query_coverage': query_coverage
     }
